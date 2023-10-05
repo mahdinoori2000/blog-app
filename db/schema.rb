e20b4f71328d3b6dfa000b40bbee05f5722ddc4c
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-05 15:34:01 +0430

    Add comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_103128) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_105914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id_id", null: false
+    t.bigint "post_id_id", null: false
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id_id"], name: "index_comments_on_post_id_id"
+    t.index ["user_id_id"], name: "index_comments_on_user_id_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "author_id", null: false
@@ -34,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_103128) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts", column: "post_id_id"
+  add_foreign_key "comments", "users", column: "user_id_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
