c83375a91156503be495d7f6e989433817e9c8db
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-07 16:09:28 +0430

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,17 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: "User" 
-    has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-    def five_recent_comments
-        comments.order(created_at: :dec).limit(5)
-    end
-    def update_comment_counter
-        update(comments_counter: comments.size)
-    end
-    def update_like_counter
-        update(likes_counter: likes.size)
-    end
+  def five_recent_comments
+    comments.order(created_at: :dec).limit(5)
+  end
+
+  def update_comment_counter
+    update(comments_counter: comments.size)
+  end
+
+  def update_like_counter
+    update(likes_counter: likes.size)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: :author_id, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-    def three_recent_posts
-        posts.order(created_at: :dec).limit(3)
-    end
+  def three_recent_posts
+    posts.order(created_at: :dec).limit(3)
+  end
 
-    def update_post_counter
-        update(posts_counter: posts.size)
-    end
+  def update_post_counter
+    update(posts_counter: posts.size)
+  end
 end
