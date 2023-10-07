cd401315b291760391a20b9b0863ed768cff7088
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-07 15:57:02 +0430

    data proccessed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user, class: "User" 
-    has_many :likes
-    has_many :comments
+    belongs_to :author, class_name: "User" 
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
 end
