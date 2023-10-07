b6350ddf70c99fe27d71ec03e687a462055247d0
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-07 16:05:29 +0430

    Add additional methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: "User" 
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
+
+    def five_recent_comments
+        comments.order(created_at: :dec).limit(5)
+    end
+    def update_comment_counter
+        update(comments_counter: comments.size)
+    end
+    def update_like_counter
+        update(likes_counter: likes.size)
+    end
 end
