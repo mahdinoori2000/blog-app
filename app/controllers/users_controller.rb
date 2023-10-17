517a4723704124b975b53ec3e704b9070dbaa9ff
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-17 11:27:41 +0430

    Add controller for posts and single post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,32 @@ class UsersController < ApplicationController
   def index
   end
 
-  def show
+  def user
     user_id = params[:id]
     @user = {
       id: user_id,
       name: "mahdi noori",
       bio: "This is the bio of the user",
+    }
+  end
+
+  def posts
+    user_id = params[:id]
+    @posts = {
+      id: user_id,
+      post_text: 'this is the post text',
+      likes: 5,
+      comments: 8,
+    }
+  end
+  def post 
+    post_id = params[:post_id]
+    user_id = params[:id]
+    @post = {
+      post_id: post_id,
+      user_id: user_id,
+      post_text: 'this is post text',
+      comments: 5,
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get "/users", to: "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#user'
+  get '/users/:id/posts', to: 'users#posts'
+  get 'users/:id/posts/:post_id', to: 'users#post'
 end
