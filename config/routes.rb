44a014a64e70d91794b27d16b72c5740e7b264a7
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-18 13:48:55 +0430

    Add resources in the router for defining routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get "/users", to: "users#index"
   # Defines the root path route ("/")
   root "users#index"
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/posts', to: 'posts#index'
-  get 'users/:id/posts/:post_id', to: 'posts#show'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
