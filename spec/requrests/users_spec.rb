037d76c4990e117c37b0e6ada8da83e9045da394
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-19 17:54:57 +0430

    Remove unused code

--- spec/requrests/users_spec.rb
+++ spec/requrests/users_spec.rb
@@ -15,8 +15,7 @@ RSpec.describe UsersController, type: :controller do
 
   describe 'GET show' do
     it 'returns http success' do
-      # Simulate a request to the show action with a user ID
-      get :show, params: { id: 1 } # Replace 1 with the desired user ID
+      get :show, params: { id: 1 }
       expect(response).to have_http_status(:success)
     end
 
@@ -24,7 +23,7 @@ RSpec.describe UsersController, type: :controller do
         user_id = 1
         get :show, params: { id: user_id }
         expect(assigns(:user)).to eq({
-          id: user_id.to_s, # Convert user_id to a string
+          id: user_id.to_s,
           name: 'mahdi noori',
           bio: 'This is the bio of the user'
         })
