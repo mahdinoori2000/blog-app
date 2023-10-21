b1a8a8035d362ab4c139c936217b606597defb69
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-21 20:39:49 +0430

    Add code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,25 @@ class PostsController < ApplicationController
     @user = User.find(user_id)
     @post = @user.posts.find(params[:id])
   end
+
+  def new
+    @current_user = current_user
+    @post = @current_user.posts.new
+  end
+
+  def create
+    @current_user = current_user
+    @post = current_user.posts.new(post_params.merge(comments_counter: 0, likes_counter: 0))
+    if @post.save
+      redirect_to user_post_url(@current_user, @post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
