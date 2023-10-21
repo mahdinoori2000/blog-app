d9846b1819e2110075da25cce42df9701572024a
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-21 22:20:45 +0430

    Fix route problem

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
-    end
-  end
+    end
+  end
 end
