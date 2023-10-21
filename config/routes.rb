c90348ea16916de4955028e5c0419834c27da56b
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-21 22:18:54 +0430

    Fix comment issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'users#index'
  
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
-  end
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+    end
+  end
 end
