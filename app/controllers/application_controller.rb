8b23162c7fc9ba20c5d431e33259afcd943534ce
Mahdi Noori <mahdinoori2389@gmail.com>
2023-10-26 17:50:32 +0430

    Add devise gem to the project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :current_user
 
-  def current_user
-    @current_user = User.first
-  end
 end
