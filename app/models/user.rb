3ec2728dc2e0904d88d6c5c4a57c1ede7c9f30fc
Alfred M <alfred.mkg@proton.me>
2023-10-26 16:25:21 +0300

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
 
   validates :name, presence: true, allow_blank: false, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, allow_blank: false
   validates :post_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
