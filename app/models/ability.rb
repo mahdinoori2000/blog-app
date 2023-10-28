3062027181e9e236e1d3ea85c5f8e0e6c3269984
Alfred M <alfred.mkg@proton.me>
2023-10-28 08:49:53 +0300

    Remove boilerplate in ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #   def initialize(user)
     user ||= current_user
 
     if user.role == "admin"
@@ -17,29 +15,5 @@ class Ability
       can :destroy, Post, user_id: user.id
       can :destroy, Comment, user_id: user.id
     end
-  end
-
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
   end
 end
